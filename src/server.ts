a81fbd0a94dc29b55de2a3285e84bd4bcdd86697
Luis Hey <luis.dragaohey@hotmail.com>
2026-08-11 21:38:44 -0300

    Mini atualização

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,34 @@ import http from "node:http";
 
 const port = 3000;
 
+const routes = {
+    "/": {
+        message: "Hotelaria Hostelix API",
+    },
+
+    "/categories": {
+        message: "Categorias",
+    },
+
+    "/products": {
+        message: "Produtos",
+    },
+};
+
 const server = http.createServer((req, res) => {
-    const response = {
-        message: "API Hotel",
-        version: "1.0.0",
-        outros: "XPTO"
-    };
+    const response = routes[req.url as keyof typeof routes];
+
+    if (!response) {
+        res.writeHead(404, {
+            "content-type": "application/json",
+        });
+
+        return res.end(
+            JSON.stringify({
+                message: "Rota não encontrada",
+            }),
+        );
+    }
 
     res.writeHead(200, {
         "content-type": "application/json",
@@ -17,5 +39,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, () => {
-    console.log(`Servidor executando em http://localhost:`$(port));
+    console.log(`Servidor executando em http://localhost:${port}`);
 });
